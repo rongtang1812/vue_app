e257d5d51655cf410dc4ee14b503427a85e7cf02
rongtang1812 <rongtang@sina.cn>
2020-08-19 22:25:46 +0800

    819提交

--- src/main.js
+++ src/main.js
@@ -3,13 +3,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
 
 Vue.config.productionTip = false
 
+Vue.use(MintUI)
+
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
-})
+  render: h => h(App)
+}).$mount('#app')
